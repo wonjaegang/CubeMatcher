9bf1e53855df73018e3854e02f20c5c2dc4b0819
Wonjaegang <robinsam@naver.com>
2021-07-25 02:12:56 +0900

    Add rotate method to Plane class
    -Plane 클래스에 rotate 메서드를 추가하였다. 해당 면의 법선벡터에 해당하는 축이 회전할 때 면의 색 배열 상태를 회전시켜주는 메서드로, 회전변환을 활용하였다.

--- Cube_Matcher.py
+++ Cube_Matcher.py
@@ -1,8 +1,15 @@
+from math import *
+import copy
+
+
 class Axis:
     def __init__(self, vector):
         self.unitVector = vector
 
-    def rotate(self, target, angle):
+    def rotate(self, depth, angle):
+        for p in planeList:
+            if p.normalVector == self.unitVector:
+                p.rotate(angle)
         pass
 
 
@@ -10,10 +17,17 @@ class Plane:
     def __init__(self, normalVector, upperVector):
         self.normalVector = normalVector
         self.upperVector = upperVector
-        self.state = [[0 for i in range(cubeSize)] for i in range(cubeSize)]
+        # self.state = [[0 for i in range(cubeSize)] for i in range(cubeSize)]
+        self.state = [[1, 0, 0, 0], [2, 0, 0, 0], [3, 0, 0, 0], [4, 0, 0, 0]]
 
-    def initializeState(self):
-        pass
+    def rotate(self, angle):
+        tempState = copy.deepcopy(self.state)
+        trans = (cubeSize - 1) / 2
+        for column in range(cubeSize):
+            for row in range(cubeSize):
+                newRow = int((row - trans) * cos(angle) + (column - trans) * sin(angle) + trans + 0.5)
+                newColumn = int(-(row - trans) * sin(angle) + (column - trans) * cos(angle) + trans + 0.5)
+                self.state[newColumn][newRow] = tempState[column][row]
 
     def printState(self):
         for column in range(cubeSize):
@@ -39,11 +53,14 @@ def printCubeState():
 
 
 if __name__ == "__main__":
-    cubeSize = 3
+    cubeSize = 4
     unitVector = UnitVector()
     axisX = Axis(unitVector.x)
     axisY = Axis(unitVector.y)
     axisZ = Axis(unitVector.z)
+    axisMX = Axis(unitVector.mx)
+    axisMY = Axis(unitVector.my)
+    axisMZ = Axis(unitVector.mz)
 
     plane1 = Plane(unitVector.x, unitVector.z)
     plane2 = Plane(unitVector.mx, unitVector.z)
@@ -55,3 +72,6 @@ if __name__ == "__main__":
 
     for plane in planeList:
         plane.printState()
+
+    plane1.rotate(pi/2)
+    plane1.printState()
