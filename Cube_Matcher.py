83592965e5290b73464a1828ebf5008834bc430d
Wonjaegang <robinsam@naver.com>
2021-07-25 00:50:08 +0900

    Add unitVector class, Add upperVector parameter to Plane class
    -unitVector 클래스를 통해 단위기저벡터들의 사용이 조금 더 용이하고 가독성이 높아지도록 했다.
    -Plane 클래스에 upperVector 파라미터를 추가하여 state의 방향을 고정시켜주었다.

--- Cube_Matcher.py
+++ Cube_Matcher.py
@@ -1,22 +1,37 @@
 class Axis:
-    def __init__(self, unitVector):
-        self.unitVector = unitVector
+    def __init__(self, vector):
+        self.unitVector = vector
 
     def rotate(self, target, angle):
         pass
 
 
 class Plane:
-    def __init__(self, normalVector):
+    def __init__(self, normalVector, upperVector):
         self.normalVector = normalVector
+        self.upperVector = upperVector
         self.state = [[0 for i in range(cubeSize)] for i in range(cubeSize)]
+
+    def initializeState(self):
+        pass
 
     def printState(self):
         for column in range(cubeSize):
             for row in range(cubeSize):
                 print("%d " % self.state[column][row], end='')
             print()
-        print("Plane normal vector:", self.normalVector, "\n")
+        print("Plane normal vector:", self.normalVector)
+        print("Plane upper vector:", self.upperVector, "\n")
+
+
+class UnitVector:
+    def __init__(self):
+        self.x = [1, 0, 0]
+        self.mx = [-1, 0, 0]
+        self.y = [0, 1, 0]
+        self.my = [0, -1, 0]
+        self.z = [0, 0, 1]
+        self.mz = [0, 0, -1]
 
 
 def printCubeState():
@@ -25,15 +40,17 @@ def printCubeState():
 
 if __name__ == "__main__":
     cubeSize = 3
-    axisX = Axis([1, 0, 0])
-    axisY = Axis([0, 1, 0])
-    axisZ = Axis([0, 0, 1])
-    plane1 = Plane([1, 0,  0])
-    plane2 = Plane([-1, 0, 0])
-    plane3 = Plane([0, 1,  0])
-    plane4 = Plane([0, -1, 0])
-    plane5 = Plane([0, 0,  1])
-    plane6 = Plane([0, 0, -1])
+    unitVector = UnitVector()
+    axisX = Axis(unitVector.x)
+    axisY = Axis(unitVector.y)
+    axisZ = Axis(unitVector.z)
+
+    plane1 = Plane(unitVector.x, unitVector.z)
+    plane2 = Plane(unitVector.mx, unitVector.z)
+    plane3 = Plane(unitVector.y, unitVector.z)
+    plane4 = Plane(unitVector.my, unitVector.x)
+    plane5 = Plane(unitVector.z, unitVector.mx)
+    plane6 = Plane(unitVector.mz, unitVector.mx)
     planeList = [plane1, plane2, plane3, plane4, plane5, plane6]
 
     for plane in planeList:
