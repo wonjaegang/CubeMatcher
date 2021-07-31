c6aaab0db6a3b1e5d27c48fed0b6663ee08d1c6d
Wonjaegang <robinsam@naver.com>
2021-07-31 21:18:26 +0900

    Add rotateLocation method to piece class
    - 큐브 회전 시, 각 조각들은 위치와 방향이 변화한다. 그 중 위치변화에 대한 메서드로, 회전 축이 큐브 원점에 있도록 평행이동 후, 회전변환 이후 다시 원 위치로 평행이동시킨다.

--- Cube_Matcher2.py
+++ Cube_Matcher2.py
@@ -9,7 +9,24 @@ class Piece:
                            'G' if location[1] == 0 else 'E',
                            'Y' if location[2] == 0 else 'E']
 
+    # 큐브 회전 시 조각의 위치변환
+    def rotateLocation(self, rotateAxis, direction):
+        def rotate90Matrix(x, y, z):
+            newX = rotateAxis[0] * x + rotateAxis[1] * (direction * z) + rotateAxis[2] * (-direction * y)
+            newY = rotateAxis[0] * (-direction * z) + rotateAxis[1] * y + rotateAxis[2] * (direction * x)
+            newZ = rotateAxis[0] * (direction * y) + rotateAxis[1] * (-direction * x) + rotateAxis[2] * z
+            return [newX, newY, newZ]
 
+        trans = (cubeSize - 1) / 2
+        temp = list(map(lambda x: x - trans, self.location))
+        self.location = list(map(lambda x: int(x + trans), rotate90Matrix(temp[0], temp[1], temp[2])))
+
+    # 큐브 회전 시 조각의 색 방향 변환
+    def rotateColorState(self, rotateAxis, direction):
+        pass
+
+
+# Piece 객체리스트로 부터 큐브의 면 정도 추출
 def getPlaneArray():
     planeArray = []
     for direction in range(6):
@@ -18,12 +35,39 @@ def getPlaneArray():
     return planeArray
 
 
+def rotate(axis, target, direction):
+    pass
+
+
 if __name__ == "__main__":
-    cubeSize = 3
+    cubeSize = 2
 
     pieces = [Piece([i, j, k]) for i in range(cubeSize) for j in range(cubeSize) for k in range(cubeSize)]
     for i in pieces:
         print("location:", i.location)
         print("color state:", i.colorState)
         print("=" * 50)
+
     print(getPlaneArray())
+
+    print(pieces[7].location)
+    pieces[7].rotateLocation([1, 0, 0], 1)
+    print(pieces[7].location)
+    pieces[7].rotateLocation([1, 0, 0], 1)
+    print(pieces[7].location)
+    pieces[7].rotateLocation([1, 0, 0], 1)
+    print(pieces[7].location)
+    pieces[7].rotateLocation([1, 0, 0], 1)
+    print(pieces[7].location)
+    print("\n")
+
+    print(pieces[0].location)
+    pieces[0].rotateLocation([0, 1, 0], -1)
+    print(pieces[0].location)
+    pieces[0].rotateLocation([0, 1, 0], -1)
+    print(pieces[0].location)
+    pieces[0].rotateLocation([0, 1, 0], -1)
+    print(pieces[0].location)
+    pieces[0].rotateLocation([0, 1, 0], -1)
+    print(pieces[0].location)
+
