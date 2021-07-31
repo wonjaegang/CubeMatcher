87a30cc7a944c858eee2950ca6084a7ebfc8da7b
Wonjaegang <robinsam@naver.com>
2021-07-31 14:21:03 +0900

    Add initializing method to Piece class & add getPlaneArray function
    - Piece 클래스의 colorState 를 초기화하는 코드를 추가하였다.
    - piece 객체리스트 pieces 에서 큐브 각 면의 색 배열을 추출하는 getPlaneArray 함수를 추가하였다.

--- Cube_Matcher2.py
+++ Cube_Matcher2.py
@@ -1,11 +1,29 @@
 class Piece:
+    # location: [x, y, z], colorState = [x, y, z, -x, -y, -z]
     def __init__(self, location):
-        self.colorState = {'x': 0, 'y': 0, 'z': 0, '-x': 0, '-y': 0, '-z': 0}
-        self.location = {'x': location[0], 'y': location[1], 'z': location[2]}
+        self.location = location
+        self.colorState = ['R' if location[0] == cubeSize - 1 else 'E',
+                           'B' if location[1] == cubeSize - 1 else 'E',
+                           'W' if location[2] == cubeSize - 1 else 'E',
+                           'O' if location[0] == 0 else 'E',
+                           'G' if location[1] == 0 else 'E',
+                           'Y' if location[2] == 0 else 'E']
+
+
+def getPlaneArray():
+    planeArray = []
+    for direction in range(6):
+        planeArray.append([piece.colorState[direction] for piece in pieces
+                           if piece.location[direction % 3] == (cubeSize - 1) * (1 - direction // 3)])
+    return planeArray
 
 
 if __name__ == "__main__":
     cubeSize = 3
+
     pieces = [Piece([i, j, k]) for i in range(cubeSize) for j in range(cubeSize) for k in range(cubeSize)]
     for i in pieces:
-        print(i.location)
+        print("location:", i.location)
+        print("color state:", i.colorState)
+        print("=" * 50)
+    print(getPlaneArray())
